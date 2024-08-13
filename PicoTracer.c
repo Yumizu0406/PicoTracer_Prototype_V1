33030b907629b9f1bfe59d1641cf31eccb8fa407
yuusukeShimizu <shimizu.yasurikanazuti@gmail.com>
2024-08-13 20:42:40 +0900

    スイッチ処理を追加

--- PicoTracer.c
+++ PicoTracer.c
@@ -1,17 +1,28 @@
 #include "pico/stdlib.h"
 #include "driver/timer.h"
+#include "driver/sw.h"
 
 void init_driver(void);
 
 int main() {
 
+    gpio_init(5);
+    gpio_set_dir(5, GPIO_OUT);
+
     init_driver();
 
     while (true) {
+        if ( isSwStatus(SW_EXE, push) ){
+            gpio_put(5, 0);
+        }
+        else{
+            gpio_put(5, 1);
+        }
 
     }
 }
 
 void init_driver(void){
+    init_sw();
     init_timer();
 }
